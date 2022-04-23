f470e61f30437f64e3823fc064528d39b5e0af9d
wiffin <wiffinian@gmail.com>
2022-04-23 10:52:53 -0400

    add functionality to navbar set up for loggedin and loggedout user

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,17 +1,48 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
+import Auth from '../utils/auth';
+
 const AppNavbar = () => {
 
   return (
     <>
       <header className="App-header">
-        <h1 className="headerh1">.shop()</h1>
+        <h1 className="headerh1">
+          <Link to='/'>.shop()</Link> 
+        </h1>
         <div className="navparent">
-          <nav className="navheader activenav">SHOP</nav>
-          <nav className="navheader">ACCOUNT</nav>
+          <nav className="navheader activenav">
+            <Link to='/'>SHOP</Link> 
+          </nav>
+          {Auth.loggedIn() ? (
+            <>
+              <nav className="navheader">
+                <Link to='/user'>ACCOUNT</Link>
+              </nav>
+              <nav className="navheader">
+                <Link to='/cart'>CART</Link>
+              </nav>
+              <nav className="navheader">
+                <Link onClick={Auth.logout}>LOGOUT</Link>
+              </nav>
+            </>
+          ) : (
+            <>
+              <nav className="navheader">
+                <Link to='/login'>CART</Link>
+              </nav>
+              <nav className="navheader">
+                <Link to='/login'>LOGIN</Link>
+              </nav>
+              <nav className="navheader">
+                <Link to='/signup'>SIGN-UP</Link>
+              </nav>
+            </>
+          )}
+          {/* <nav className="navheader">ACCOUNT</nav>
           <nav className="navheader">CART</nav>
-          <nav className="navheader">LOGOUT</nav>
+          <nav className="navheader">LOGOUT</nav> */}
         </div>
       </header>
     </>
