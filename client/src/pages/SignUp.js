ba8980726ba92dd548c709f07a73793783a24435
Tinastouch <tinastouch@gmail.com>
2022-04-23 14:15:22 -0400

    login signup and arrows

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { useMutation } from '@apollo/client';
+import Auth from '../utils/auth';
+import { ADD_USER } from '../utils/mutations';
 
-const SignUp = () => {
+// const SignUp = () => {
+
+    function SignUp(props) {
+        const [formState, setFormState] = useState({ email: '', password: '' });
+        const [addUser] = useMutation(ADD_USER);
+      
+        const handleFormSubmit = async (event) => {
+          event.preventDefault();
+
+          console.log(formState)
+          const mutationResponse = await addUser({
+            variables: {
+              email: formState.email,
+              password: formState.password,
+            },
+          });
+          const token = mutationResponse.data.addUser.token;
+          Auth.login(token);
+        };
+      
+        const handleChange = (event) => {
+          const { name, value } = event.target;
+          setFormState({
+            ...formState,
+            [name]: value,
+          });
+        };
 
     return (
         <>
@@ -13,18 +42,36 @@ const SignUp = () => {
     
                 <div className="subnav-parent"></div>
     
-                <form className="signup-form">
+                <form onSubmit={handleFormSubmit} className="signup-form">
         <h2 className="tf-h2">sign up</h2> 
         <div className="form-group">
-            <input type="text" className="form-input" spellcheck="false"></input>
+            <input 
+            type="text" 
+            className="form-input" 
+            spellcheck="false"
+            id="email"
+            name="email"
+            placeholder="email"
+            onChange={handleChange}></input>
             <label className="form-label">E-MAIL</label>
         </div>
         <div className="form-group">
-            <input type="password" className="form-input" spellcheck="false"></input>
+            <input type="password" 
+            className="form-input" 
+            spellcheck="false"
+            id="password"
+            name="password"
+            placeholder="*****"
+            onChange={handleChange}></input>
             <label className="form-label">PASSWORD</label>
         </div>
         <div className="form-group">
-            <input type="password" className="form-input" spellcheck="false"></input>
+            <input type="password" 
+            className="form-input" 
+            spellcheck="false"
+            id="confirm-password"
+            placeholder="*****"
+            onChange={handleChange}></input>
             <label className="form-label">PASSWORD CONFIRM</label>
         </div>
         <input type="submit" className="submit-btn"></input>
