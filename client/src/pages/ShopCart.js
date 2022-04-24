dff4508ec0e077855e68cbb64b9ab779335dd19a
Steve Dusome <stevedusome@gmail.com>
2022-04-24 07:41:41 -0400

    added cart page xml and css

--- client/src/pages/ShopCart.js
+++ client/src/pages/ShopCart.js
@@ -14,6 +14,7 @@ const ShopCart = () => {
 
     <form className="login-form change-password">
         <h2 className="tf-h2">CART</h2>
+
         <div className="row">
         <div className="form-group">
             <p className="form-input" spellcheck="false">STEVEDUSOME@GMAIL.COM</p>
@@ -24,7 +25,11 @@ const ShopCart = () => {
             <label className="form-label">PRICE</label>
         </div>
         <div className="form-group">
-        <p className="form-input cart-cat">3</p>
+        <p className="form-input cart-cat"> 
+        <button className="qty-btn"><p className="negativemarg">-</p></button> 
+        3
+        <button className="qty-btn"><p className="negativemarg">+</p></button> 
+        </p>
             <label className="form-label">QUANTITY</label>
         </div>
         <div className="form-group">
@@ -32,6 +37,61 @@ const ShopCart = () => {
             <label className="form-label">TOTAL</label>
         </div>
         <button type="delete" className="add-to-cart">DELETE ITEM</button></div> 
+
+        <div className="row">
+        <div className="form-group">
+            <p className="form-input" spellcheck="false">STEVEDUSOME@GMAIL.COM</p>
+            <label className="form-label">ITEM NAME</label>
+        </div>
+        <div className="form-group">
+            <p className="form-input cart-cat">5.00</p>
+            <label className="form-label">PRICE</label>
+        </div>
+        <div className="form-group">
+        <p className="form-input cart-cat"> 
+        <button className="qty-btn"><p className="negativemarg">-</p></button> 
+        3
+        <button className="qty-btn"><p className="negativemarg">+</p></button> 
+        </p>
+            <label className="form-label">QUANTITY</label>
+        </div>
+        <div className="form-group">
+            <p className="form-input cart-cat">15.00</p>
+            <label className="form-label">TOTAL</label>
+        </div>
+        <button type="delete" className="add-to-cart">DELETE ITEM</button></div> 
+
+        <div className="row">
+        <div className="form-group">
+            <p className="form-input" spellcheck="false">STEVEDUSOME@GMAIL.COM</p>
+            <label className="form-label">ITEM NAME</label>
+        </div>
+        <div className="form-group">
+            <p className="form-input cart-cat">5.00</p>
+            <label className="form-label">PRICE</label>
+        </div>
+        <div className="form-group">
+        <p className="form-input cart-cat"> 
+        <button className="qty-btn"><p className="negativemarg">-</p></button> 
+        3
+        <button className="qty-btn"><p className="negativemarg">+</p></button> 
+        </p>
+            <label className="form-label">QUANTITY</label>
+        </div>
+        <div className="form-group">
+            <p className="form-input cart-cat">15.00</p>
+            <label className="form-label">TOTAL</label>
+        </div>
+        <button type="delete" className="add-to-cart">DELETE ITEM</button></div> 
+
+        <div className="row flex-end">
+        
+        <div className="form-group">
+            <p className="form-input cart-cat">45.00</p>
+            <label className="form-label">TOTAL</label>
+        </div>
+        <button type="delete" className="add-to-cart checkout">CHECKOUT</button></div> 
+
     </form>
 
         <div className="subnav-parent">
